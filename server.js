d84eb90c17bb63d131925a616b12f41071777090
Habib <mhabibofficial@yahoo.com>
2019-06-28 11:14:17 +0700

    hayygays

--- server.js
+++ server.js
@@ -17,12 +17,12 @@ let correctPercentage = 0;
 
 
 // Return the teacher page
-app.get('/teacher', function(req, res){
+app.get('/teacher', function (req, res) {
   res.sendFile(__dirname + '/teacher.html');
 });
 
 // Return the student page
-app.get('/student', function(req, res){
+app.get('/student', function (req, res) {
   res.sendFile(__dirname + '/student.html');
 });
 
@@ -30,25 +30,40 @@ app.get('/student', function(req, res){
 var correctanswer = "";
 
 // If we have a connection...
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
 
   // If a question is submitted...
-  socket.on('submitquestion', function(quesdata){
+  socket.on('submitquestion', function (quesdata) {
+    correctAnswers = 0;
+    incorrectAnswers = 0;
+    usersCorrect = [];
+    usersIncorrect = [];
+    totalAnswers = 0;
 
     // What is the question?  log it in console for debug...
     console.log("question submitted: " + JSON.stringify(quesdata) + " " + quesdata.question + " " + quesdata.questionType);
     // Set the correct answer
     correctanswer = quesdata.answer;
     // Broadcast question to all connections except sender, i.e. except teacher
-    socket.broadcast.emit('deliverquestion', {question: quesdata.question, questionType: quesdata.questionType, timeLimit: quesdata.timeLimit});
+    socket.broadcast.emit('deliverquestion', {
+      question: quesdata.question,
+      questionType: quesdata.questionType,
+      timeLimit: quesdata.timeLimit
+
+    });
 
   });
 
   // If an answer to the question is received...
-  socket.on('answerquestion', function(answerdata) {
+  socket.on('answerquestion', function (answerdata) {
     // Send back the result, but only to the client that sent the answer
-    
-    if(answerdata.answer == correctanswer){
+
+    // correctAnswers = answerdata.correctAnswers;
+    // incorrectAnswers = answerdata.incorrectAnswers;
+    // usersCorrect.push(answerdata.usersCorrect);
+    // usersIncorrect.push(answerdata.usersIncorrect);
+    // totalAnswers = answerdata.totalAnswers;
+    if (answerdata.answer == correctanswer) {
       correctAnswers++;
       usersCorrect.push(answerdata.username);
     } else {
@@ -61,22 +76,24 @@ io.on('connection', function(socket){
     incorrectAnswers = incorrectAnswers;
     incorrectUsers = usersIncorrect;
     correctUsers = usersCorrect;
-    let percentage = (correctAnswers/totalAnswers)* 100;
+    let percentage = (correctAnswers / totalAnswers) * 100;
 
-    socket.broadcast.emit("deliverData",{totalAnswers:totalAnswers,correctAnswers:correctAnswers,incorrectAnswers:incorrectAnswers,incorrectUsers:incorrectUsers,correctUsers:correctUsers,percentage:percentage});
+    socket.broadcast.emit("deliverData", { totalAnswers: totalAnswers, correctAnswers: correctAnswers, incorrectAnswers: incorrectAnswers, incorrectUsers: incorrectUsers, correctUsers: correctUsers, percentage: percentage });
 
-    
+
     io.to(socket.id).emit("resultquestion"
-                         ,{correct: (correctanswer == answerdata.answer) 
-                         ,answer: correctanswer
-                        ,username:answerdata.username}
-                         );          
-    });
-    
-  
+      , {
+        correct: (correctanswer == answerdata.answer)
+        , answer: correctanswer
+        , username: answerdata.username
+      }
+    );
+  });
+
+
 });
 
 // Have the server listen...
-http.listen(3000, function(){
+http.listen(3000, function () {
   console.log('listening on *:3000');
 });
